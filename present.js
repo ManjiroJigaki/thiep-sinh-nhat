9010773b1a9ef5eeca94a75dd3482d6fc3d021b7
ManjiroJigaki <138122190+ManjiroJigaki@users.noreply.github.com>
2023-07-12 03:24:52 +0700

    Update present.js

--- present.js
+++ present.js
@@ -1,5 +1,5 @@
 
-var to = 'Jigaki!';
+var to = 'Kim Khánh!';
 var gift_url = 'https://www.pinterest.com/pin/14003448834600908/';
 var gift_image_url = 'Happy Birthday Pusheen Pin On Pinterest 👑👑👑.jpeg';
 
