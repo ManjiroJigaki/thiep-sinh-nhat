5035a1e575f75827b5e99b12dccd363773e475cf
ManjiroJigaki <138122190+ManjiroJigaki@users.noreply.github.com>
2023-10-07 12:37:27 +0700

    Update present.js

--- present.js
+++ present.js
@@ -1,5 +1,5 @@
 
-var to = 'Nguyễn Thi Huỳnh Như!';
+var to = 'Huỳnh Như!';
 var gift_url = 'https://www.pinterest.com/pin/14003448834600908/';
 var gift_image_url = 'Happy Birthday Pusheen Pin On Pinterest 👑👑👑.jpeg';
 
