0871cd61ac2dce04582966221ebbd06284774658
ManjiroJigaki <138122190+ManjiroJigaki@users.noreply.github.com>
2024-09-06 11:32:09 +0700

    Update present.js

--- present.js
+++ present.js
@@ -1,5 +1,5 @@
 
-var to = 'Khánh!';
+var to = 'Hồng Hạnh!';
 var gift_url = 'https://www.pinterest.com/pin/14003448834600908/';
 var gift_image_url = 'Happy Birthday Pusheen Pin On Pinterest 👑👑👑.jpeg';
 
