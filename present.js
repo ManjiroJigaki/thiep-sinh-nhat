5769f35c4783151d4e31cf055108148bb1ddf4cf
ManjiroJigaki <138122190+ManjiroJigaki@users.noreply.github.com>
2025-05-16 12:42:16 +0700

    Update present.js

--- present.js
+++ present.js
@@ -1,5 +1,5 @@
 
-var to = " Phạm Ngọc Trung " ;
+var to = " Phạm Trung " ;
 var gift_url = 'https://www.pinterest.com/pin/14003448834600908/';
 var gift_image_url = 'Happy Birthday Pusheen Pin On Pinterest 👑👑👑.jpeg';
 
