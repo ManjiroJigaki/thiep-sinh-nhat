9b426c36746b0e9dc093116b70b6493ff53e14b3
ManjiroJigaki <138122190+ManjiroJigaki@users.noreply.github.com>
2025-07-24 19:06:13 +0700

    Update present.js

--- present.js
+++ present.js
@@ -1,5 +1,5 @@
 
-var to = " Thảo Uyên " ;
+var to = " Huỳnh Văn Kiên " ;
 var gift_url = 'https://www.pinterest.com/pin/14003448834600908/';
 var gift_image_url = 'Happy Birthday Pusheen Pin On Pinterest 👑👑👑.jpeg';
 
